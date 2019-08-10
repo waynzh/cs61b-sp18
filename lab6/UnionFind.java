1bcc940ad36528962303452ea7df8a1a12d330f0
Wayne299 <673156454@qq.com>
2019-08-10 22:35:42 +0800

    init lab6 UnionFind

---
+++ lab6/UnionFind.java
@@ -0,0 +1,30 @@
+public class UnionFind{
+
+    public UnionFind(int n) {
+
+    }
+
+    public void validate(int v1) {
+        
+    }
+
+    public int sizeOf(int v1) {
+
+    }
+
+    public int parent(int v1) {
+
+    }
+
+    public boolean connected(int v1, int v2) {
+
+    }
+
+    public void union(int v1, int v2) {
+
+    }
+
+    public int find(int v1) {
+
+    }
+}
