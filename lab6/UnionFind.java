8ec9149bcbe4889e1e65b902c754e11a4b2dd121
Wayne299 <673156454@qq.com>
2019-08-11 21:54:16 +0800

    HW2 finished

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,30 +1,4 @@
-public class UnionFind{
+package PACKAGE_NAME;
 
-    public UnionFind(int n) {
-
-    }
-
-    public void validate(int v1) {
-        
-    }
-
-    public int sizeOf(int v1) {
-
-    }
-
-    public int parent(int v1) {
-
-    }
-
-    public boolean connected(int v1, int v2) {
-
-    }
-
-    public void union(int v1, int v2) {
-
-    }
-
-    public int find(int v1) {
-
-    }
+public class UnionFind {
 }
