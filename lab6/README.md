2c394f80fa3920a6f5c1cf5776214f1909ce6218
Wayne299 <673156454@qq.com>
2019-08-12 12:00:23 +0800

    ======== lab6 completed ========

---
+++ lab6/README.md
@@ -0,0 +1,4 @@
+## Lab6 Completed
+**分数**: 0 / 0
+
+- union时的size问题尚未解决。
--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
-
 public class UnionFind {
+    private int[] parent;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
+    }
+
+    private void validate(int v1) {
+        int n = parent.length;
+        if (v1 < 0 || v1 >= n) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int sizeOf(int v1) {
+        return -parent(find(v1));
+    }
+
+    public int parent(int v1) {
+        return parent[v1];
+    }
+
+    public boolean connected(int v1, int v2) {
+        return find(v1) == find(v2);
+    }
+
+    public void union(int v1, int v2) {
+        if (!connected(v1, v2)) {
+            if (sizeOf(v1) > sizeOf(v2)) {
+                parent[find(v1)] -= sizeOf(v2);   // size计算问题？
+                parent[find(v2)] = find(v1);
+            } else {
+                parent[find(v2)] -= sizeOf(v1);
+                parent[find(v1)] = find(v2);
+            }
+        }
+    }
+
+    public int find(int v1) {
+        validate(v1);
+        int root = v1;
+        while (parent(root) > -1) {
+            root = parent(root);
+        }
+        int up = parent(v1);
+        while (up != root) {
+            parent[v1] = root;
+            v1 = up;
+            up = parent(v1);
+        }
+        return root;
+    }
 }
